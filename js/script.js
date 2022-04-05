0b3c57742c393524c5aad119e3a22109994a8505
carl_e <aharah88@gmail.com>
2022-04-04 20:34:55 -0500

    Validate and capture input

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersList = document.querySelector(".guessed-letters");
 const guessButton = document.querySelector(".guess");
 const letterInput = document.querySelector("input");
 const wordInProgress = document.querySelector(".word-in-progress");
@@ -8,10 +8,10 @@ const messageArea = document.querySelector(".message");
 const replayButton = document.querySelector(".play-again");
 
 const word = "magnolia";
-
+const guessedLetters = [];
+//Set word to blank circles
 const circleLetters = function (word) {
     const arr = word.split("");
-
     const blankArr = arr.map(function (element) {
         return element = "●"
     });
@@ -19,11 +19,38 @@ const circleLetters = function (word) {
     wordInProgress.innerHTML = blankArr.join("");
 }
 
+//validate player input
+const validate = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input == "") {
+        messageArea.innerText = "Please input a letter!";
+    } else if (input.length > 1) {
+        messageArea.innerText = "This input was too long. Choose a single letter!";
+    } else if (!input.match(acceptedLetter)) {
+        messageArea.innerText = "Entry must be a valid letter!"
+    } else {
+        makeGuess(input);
+    }
+
+}
+//Capture guess to array
+const makeGuess = function (letter) {
+    const upperLetter = letter.toUpperCase();
+    if (guessedLetters.includes(upperLetter)) {
+        messageArea.innerText = "You already used this letter. Try again!";
+    } else {
+        guessedLetters.push(upperLetter);
+        console.log(guessedLetters);
+    }
+
+}
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
+    messageArea.innerText = "";
     const guessedLetter = letterInput.value;
-    console.log(guessedLetter);
+    validate(guessedLetter);
     letterInput.value = "";
+
 })
 
 circleLetters(word);
